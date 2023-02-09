45bb1ac18d88fa19bcc6abdc722859320fbda165
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-02-09 18:00:18 +0100

    generate dockeruser in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,20 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 [[ -f $WORKSPACE/devel/setup.bash ]] && source $WORKSPACE/devel/setup.bash
 [[ -f $WORKSPACE/install/setup.bash ]] && source $WORKSPACE/install/setup.bash
 
-# execute CMD
-exec "$@"
+# generate dockeruser with local user id
+if [[ $DOCKER_UID && $DOCKER_GID ]]; then
+    groupadd -g $DOCKER_GID $DOCKER_USER
+    useradd -s /bin/bash \
+            -u $DOCKER_UID \
+            -g $DOCKER_USER \
+            --create-home \
+            --home-dir /home/$DOCKER_USER \
+            --groups sudo,video \
+            --password "$(openssl passwd -1 $DOCKER_USER)" \
+            $DOCKER_USER && \
+            touch /home/$DOCKER_USER/.sudo_as_admin_successful
+    cd /home/$DOCKER_USER
+    exec gosu $DOCKER_USER "$@"
+else
+    exec "$@"
+fi
