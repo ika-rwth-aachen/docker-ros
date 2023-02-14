d63fa901cd546acd43a27a28641af05bbd478887
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-02-14 18:48:48 +0100

    create-home and symlink to ws

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,14 +12,13 @@ if [[ $DOCKER_UID && $DOCKER_GID ]]; then
     useradd -s /bin/bash \
             -u $DOCKER_UID \
             -g $DOCKER_USER \
-            --no-create-home \
+            --create-home \
             --home-dir /home/$DOCKER_USER \
             --groups sudo,video \
             --password "$(openssl passwd -1 $DOCKER_USER)" \
             $DOCKER_USER && \
             touch /home/$DOCKER_USER/.sudo_as_admin_successful
-    cp -r /etc/skel/. /home/$DOCKER_USER
-    chown $DOCKER_USER:$DOCKER_USER /home/$DOCKER_USER
+    ln -s $WORKSPACE /home/$DOCKER_USER/ws
     exec gosu $DOCKER_USER "$@"
 else
     exec "$@"
