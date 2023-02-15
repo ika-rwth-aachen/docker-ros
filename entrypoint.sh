775be09d28e16b4029253263aeff30732841a55c
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-02-15 17:42:05 +0100

    chown workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,7 @@ if [[ $DOCKER_UID && $DOCKER_GID ]]; then
             --password "$(openssl passwd -1 $DOCKER_USER)" \
             $DOCKER_USER && \
             touch /home/$DOCKER_USER/.sudo_as_admin_successful
+    chown -R $DOCKER_USER:$DOCKER_USER $WORKSPACE
     ln -s $WORKSPACE /home/$DOCKER_USER/ws
     cd /home/$DOCKER_USER/ws
     exec gosu $DOCKER_USER "$@"
