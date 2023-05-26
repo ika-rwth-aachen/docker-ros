585b4aa53b256c71a80fc526e81d0674fffab1ff
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-05-26 16:00:44 +0200

    implement build jobs in main ci script

---
+++ ci.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_ROS_PATH="$(cd -P "$(dirname "${0}")" && pwd)"
+source "${DOCKER_ROS_PATH}/build.sh"
+
+require_var() {
+    if [[ -z "${!1}" ]]; then
+        echo "Environment variable '${1}' is required"
+        exit 1
+    fi
+}
+
+open_log_group() {
+    echo "::group::${1}"
+}
+
+close_log_group() {
+    echo "::endgroup::"
+}
+
+# set defaults for optional variables
+TARGET="${TARGET:-run}"
+PLATFORM="${PLATFORM:-$(dpkg --print-architecture)}"
+
+# check for required variables
+require_var "BASE_IMAGE"
+[[ "${TARGET}" == *"run"* ]] && require_var "COMMAND"
+[[ "${TARGET}" == *"dev"* ]] && require_var "DEV_IMAGE"
+[[ "${TARGET}" == *"run"* ]] && require_var "IMAGE"
+
+# set constant variables
+CI_POSTFIX="ci"
+CI_ARCH_POSTFIX="PLATFORM"
+
+# parse (potentially) comma-separated lists to arrays
+IFS="," read -ra TARGETS <<< "${TARGET}"
+IFS="," read -ra PLATFORMS <<< "${PLATFORM}"
+unset TARGET
+unset PLATFORM
+
+# loop over targets and platforms to build images
+for PLATFORM in "${PLATFORMS[@]}"; do
+    for TARGET in "${TARGETS[@]}"; do
+        open_log_group "${TARGET}"
+        image="${IMAGE}"
+        [[ "${TARGET}" == "dev" ]] && image="${DEV_IMAGE}"
+        ci_image="${image}_${CI_POSTFIX}-${!CI_ARCH_POSTFIX}"
+        IMAGE="${ci_image}" build_image
+        close_log_group
+    done
+done
