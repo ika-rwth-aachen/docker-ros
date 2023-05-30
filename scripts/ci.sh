2a7827698ad7ca0894cef7760edb7b6b8a66e235
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-05-30 11:27:56 +0200

    use single quotes

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -16,13 +16,13 @@ require_var "IMAGE"
 DEV_IMAGE="${DEV_IMAGE:-${IMAGE}-dev}" # TODO: what if IMAGE has no TAG?
 ENABLE_IMAGE_PUSH="${ENABLE_IMAGE_PUSH:-false}"
 ENABLE_MULTIARCH_BUILD="${ENABLE_MULTIARCH_BUILD:-false}"
-IMAGE_POSTFIX="${IMAGE_POSTFIX:-"_ci"}"
+IMAGE_POSTFIX="${IMAGE_POSTFIX:-""}"
 
 # write image name for industrial_ci to output
 # TODO: GitHub-only
 industrial_ci_image="${IMAGE}"
 [[ "${TARGET}" == *"dev"* ]] && industrial_ci_image="${DEV_IMAGE}"
-industrial_ci_image="${industrial_ci_image}${IMAGE_POSTFIX}"
+[[ -n "${IMAGE_POSTFIX}" ]] && industrial_ci_image="${industrial_ci_image}${IMAGE_POSTFIX}"
 [[ "${ENABLE_MULTIARCH_BUILD}" != "true" ]] && industrial_ci_image="${industrial_ci_image}-$(dpkg --print-architecture)"
 echo "INDUSTRIAL_CI_IMAGE=${industrial_ci_image}" >> "${GITHUB_OUTPUT}"
 
