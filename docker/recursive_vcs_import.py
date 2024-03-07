fb840ea3febdd02c6bd0de4830e7c5a3b1ab090a
Amarin Klöker <amarin.kloeker@ika.rwth-aachen.de>
2024-03-07 13:37:21 +0100

    Ignore empty .repos files

--- docker/recursive_vcs_import.py
+++ docker/recursive_vcs_import.py
@@ -6,13 +6,23 @@ import sys
 from typing import List, Optional
 
 
-def findDotRepos(search_path: str, clone_path: Optional[str] = None) -> List[pathlib.Path]:
+def find_dot_repos(search_path: str, clone_path: Optional[str] = None) -> List[pathlib.Path]:
 
     repos = list(pathlib.Path(search_path).glob("**/*.repos"))
     if clone_path is not None:
         repos.extend(list(pathlib.Path(clone_path).glob("**/*.repos")))
     return repos
 
+def is_file_empty(filename):
+    """Check if file is empty, allowing blank spaces."""
+    try:
+        with open(filename, 'r') as file:
+            content = file.read().strip()
+            return len(content) == 0
+    except Exception as e:
+        print(e)
+        # Exception gets raised later
+        return False
 
 def main():
 
@@ -22,7 +32,7 @@ def main():
 
     while True:
 
-        found_repos = findDotRepos(search_path, clone_path)
+        found_repos = find_dot_repos(search_path, clone_path)
         remaining_repos = set(found_repos) - set(cloned_repos)
 
         if not remaining_repos:
@@ -32,7 +42,11 @@ def main():
         with open(str(next_repo), "r") as f:
             proc = subprocess.run(["vcs", "import", clone_path, "--recursive"], stdin=f)
             if proc.returncode != 0:
-                raise RuntimeError("vcs import failed")
+                # Ignore empty .repos files
+                if not is_file_empty(next_repo):
+                    raise RuntimeError("vcs import failed")
+                else:
+                    print(f"Ignored empty .repos file: {next_repo}")
 
         cloned_repos.append(next_repo)
 
@@ -41,3 +55,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
